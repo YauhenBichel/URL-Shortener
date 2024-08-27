7bd37810cc327e49421452af7fdf9884200b7973
Yauhen Bichel <ccaeybi@ucl.ac.uk>
2024-08-27 12:12:44 +0100

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <div className="App">
+        <p>
+          Welcome
+        </p>
+      </div>
+  );
+}
+
+export default App;
